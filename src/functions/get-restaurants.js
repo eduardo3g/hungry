a6d338f5529c78a2065b4aea166591b53aa6d833
eduardo3g <eduardosbrasil10@gmail.com>
2021-08-30 16:20:18 -0300

    feat: implement Lambda handler to scan restaurants from DynamoDB restaurants table

---
+++ src/functions/get-restaurants.js
@@ -0,0 +1,32 @@
+const DocumentClient = require('aws-sdk/clients/dynamodb');
+
+const dynamodb = new DocumentClient();
+
+const defaultResults = process.env.DEFAULT_RESULTS || 8;
+const tableName = process.env.RESTAURANTS_TABLE;
+
+const getRestaurants = async limit => {
+  console.log(`fetching ${limit} restaurants from ${tableName}`);
+
+  const request = {
+    TableName: tableName,
+    Limit: limit,
+  };
+
+  const response = await dynamodb.scan(request).promise();
+  console.log(`found ${response.Items.length} restaurants`);
+
+  return response.Items;
+};
+
+// eslint-disable-next-line no-unused-vars
+module.exports.handler = async (event, context) => {
+  const restaurants = await getRestaurants(defaultResults);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(restaurants),
+  };
+
+  return response;
+};
