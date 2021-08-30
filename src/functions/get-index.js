e0e3c634eb18aa53b1b612b6be2623138c3f3709
eduardo3g <eduardosbrasil10@gmail.com>
2021-08-30 17:29:21 -0300

    refactor(get-index): display the restaurants list from DynamoDB on front-end

--- src/functions/get-index.js
+++ src/functions/get-index.js
@@ -1,13 +1,49 @@
-/* eslint-disable no-unused-vars */
 const fs = require('fs');
 const path = require('path');
+const Mustache = require('mustache');
+const http = require('axios');
 
-const html = fs.readFileSync(
-  path.resolve(__dirname, '..', 'static', 'index.html'),
-  'utf-8',
-);
+const restaurantsApiRoot = process.env.RESTAURANTS_API;
 
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+let html;
+
+function loadHtml() {
+  if (!html) {
+    console.log('loading index.html...');
+    html = fs.readFileSync(
+      path.resolve(__dirname, '..', 'static', 'index.html'),
+      'utf-8',
+    );
+    console.log('loaded index.html');
+  }
+
+  return html;
+}
+
+const getRestaurants = async () => {
+  const httpRequest = http.get(restaurantsApiRoot);
+  return (await httpRequest).data;
+};
+
+// eslint-disable-next-line no-unused-vars
 module.exports.handler = async (event, context) => {
+  const template = loadHtml();
+  const restaurants = await getRestaurants();
+  console.log(`restaurants`, restaurants);
+  const dayOfWeek = days[new Date().getDay()];
+  const html = Mustache.render(template, { dayOfWeek, restaurants });
+  console.log('html', html);
+
   const response = {
     statusCode: 200,
     headers: {
