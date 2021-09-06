9c3a3754a5a6f324f9dc0cd481f3dc34b38357d5
eduardo3g <eduardosbrasil10@gmail.com>
2021-09-06 10:57:06 -0300

    chore(test): implement helpers to toggle test between local handler or deployed API

--- src/__tests__/steps/when.js
+++ src/__tests__/steps/when.js
@@ -1,6 +1,10 @@
 const _ = require('lodash');
+const aws4 = require('aws4');
+const URL = require('url');
+const http = require('axios');
 
 const APP_ROOT = '../../';
+const mode = process.env.TEST_MODE;
 
 const viaHandler = async (event, functionName) => {
   const { handler } = require(`${APP_ROOT}/functions/${functionName}`);
@@ -9,7 +13,7 @@ const viaHandler = async (event, functionName) => {
   const response = await handler(event, context);
   const contentType = _.get(
     response,
-    'headers.Content-Type',
+    'headers.content-type',
     'application/json',
   );
 
@@ -20,7 +24,70 @@ const viaHandler = async (event, functionName) => {
   return response;
 };
 
-const we_invoke_get_index = () => viaHandler({}, 'get-index');
+const respondFrom = async httpResponse => ({
+  statusCode: httpResponse.status,
+  body: httpResponse.data,
+  headers: httpResponse.headers,
+});
+
+const signHttpRequest = url => {
+  const urlData = URL.parse(url);
+  const options = {
+    host: urlData.hostname,
+    path: urlData.pathname,
+  };
+
+  aws4.sign(options);
+  return options.headers;
+};
+
+const viaHttp = async (relativePath, method, options) => {
+  const url = `${process.env.REST_API_URL}/${relativePath}`;
+  console.info(`invoking via HTTP ${method} ${url}`);
+
+  try {
+    const data = _.get(options, 'body');
+    let headers = {};
+
+    if (_.get(options, 'iam_auth', false) === true) {
+      headers = signHttpRequest(url);
+    }
+
+    const authHeader = _.get(options, 'auth');
+    if (authHeader) {
+      headers.Authorization = authHeader;
+    }
+
+    const httpReq = http.request({
+      method,
+      url,
+      headers,
+      data,
+    });
+
+    const res = await httpReq;
+    return respondFrom(res);
+  } catch (e) {
+    if (e.status) {
+      return {
+        statusCode: e.status,
+        headers: e.response.headers,
+      };
+    }
+    throw e;
+  }
+};
+
+const we_invoke_get_index = async () => {
+  switch (mode) {
+    case 'handler':
+      return await viaHandler({}, 'get-index');
+    case 'http':
+      return await viaHttp('', 'GET');
+    default:
+      throw new Error(`unsupported mode: ${mode}`);
+  }
+};
 const we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants');
 
 const we_invoke_search_restaurants = theme => {
