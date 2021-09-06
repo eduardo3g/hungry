153b32abce2416e0b18684ded90753443c51735f
eduardo3g <eduardosbrasil10@gmail.com>
2021-09-06 10:58:21 -0300

    test(acceptance): implement acceptance tests fir get-restaurants endpoint

--- src/__tests__/steps/when.js
+++ src/__tests__/steps/when.js
@@ -88,7 +88,16 @@ const we_invoke_get_index = async () => {
       throw new Error(`unsupported mode: ${mode}`);
   }
 };
-const we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants');
+const we_invoke_get_restaurants = async () => {
+  switch (mode) {
+    case 'handler':
+      return await viaHandler({}, 'get-restaurants');
+    case 'http':
+      return await viaHttp('restaurants', 'GET', { iam_auth: true });
+    default:
+      throw new Error(`unsupported mode: ${mode}`);
+  }
+};
 
 const we_invoke_search_restaurants = theme => {
   let event = {
