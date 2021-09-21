34d73bb2b0e02bae849b2ce05a2355c42bae1bb3
eduardo3g <eduardosbrasil10@gmail.com>
2021-09-21 14:36:43 -0300

    feat: forward correlation IDs through EventBridge events

--- src/functions/place-order.js
+++ src/functions/place-order.js
@@ -1,9 +1,10 @@
-const EventBridge = require('aws-sdk/clients/eventbridge');
 const XRay = require('aws-xray-sdk-core');
 const Log = require('@dazn/lambda-powertools-logger');
 const wrap = require('@dazn/lambda-powertools-pattern-basic');
 
-const eventBridge = XRay.captureAWSClient(new EventBridge());
+const eventBridge = XRay.captureAWSClient(
+  require('@dazn/lambda-powertools-eventbridge-client'),
+);
 const chance = require('chance').Chance();
 
 const { BUS_NAME } = process.env;
